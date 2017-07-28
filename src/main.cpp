45af18ed17fc7fc446ea58b05ac79346623fb64c
Naxulanth <Naxulanth@gmail.com>
2017-07-28 21:06:47 +0300

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ int pct2;
 int d_pct1;
 int d_pct2;
 char* sw;
-const float color[] = { 150.,150.,150. };
 
 int main(int argc, char* argv[]) {
 
@@ -31,33 +30,46 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 
+		// check if file exists
 		if (!(boost::filesystem::is_regular_file(s_in))) {
 			std::cout << "File not found!" << std::endl;
 			break;
 		}
+
 		boost::filesystem::path path(s_in);
+		// extension
 		std::string ext = boost::filesystem::extension(s_in);
+		// filename without the extension
 		std::string pre = path.stem().string();
+		// switch in string form so that it could be used in if/else cases.
 		std::string r_sw = sw;
+		// store resize values in order to be used in new filename
 		d_pct1 = pct1;
 		d_pct2 = pct2;
 
+		// switch between percentage scaling and pixel scaling, convert numbers accordingly for it to work with the CImg resize function
+		// CImg resize function uses negative numbers (0 to -100) for percentage scaling, and positive numbers for pixel scaling.
+
+		// Ensure that the given numbers can't be less than 0 if pixel scaling is chosen.
 		if (r_sw == "-p") {
 			if (pct1 < 0) pct1 = pct1 - pct1 * 2;
 			if (pct2 < 0) pct2 = pct2 - pct2 * 2;
 			s_in = pre + "_" + std::to_string(d_pct1) + "px_" + std::to_string(d_pct2) + "px" + ext;
 		}
 
+		// Convert positive numbers to negative numbers since positive numbers are for pixel scaling, and we need negative numbers for percentage scaling.
 		else {
 			if (pct1 > 0) pct1 = pct1 - pct1 * 2;
 			if (pct2 > 0) pct2 = pct2 - pct2 * 2;
 			s_in = pre + "_" + std::to_string(d_pct1) + "pct_" + std::to_string(d_pct2) + "pct" + ext;
 		}
 	
-
+		// new name in char* format for CImg save function
 		newname = s_in.c_str();
 
-		CImg<unsigned char> image(in);
+		// create a CImg object with the selected image, resize, normalize, and save it. -100 is default value for
+		// Z and V axis scaling, 3 is for linear interpolation as resizing type.
+		 CImg<unsigned char> image(in);
 		 image.resize(pct1, pct2, -100, -100, 3);
 		 image.normalize(0, 255);
 		 image.save(newname);
