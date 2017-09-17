c64171edea08682a5a2bd3cba7618f422ad4b8a5
Naxulanth <Naxulanth@gmail.com>
2017-09-17 19:51:30 +0300

    png support

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,8 +1,10 @@
 #pragma once
 // add jpg support to CImg, note that this requires the libjpeg library to work
 #define cimg_use_jpeg
+#define cimg_use_png
+#include "CImg.h"
+#include <jpeglib.h>
+#include <png.h>
 #include <iostream>
 #include <string>
 #include <boost/filesystem.hpp>
-#include "CImg.h"
-
